b8794d2e3cf0347aad7789f0d627b1aee4fd70fb
techdave <klatzdavie19@gmail.com>
2022-04-09 15:36:02 +0300

    build category schema

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,5 +1,16 @@
 import mongoose from 'mongoose';
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
 
 export default mongoose.model('Category', categorySchema);
