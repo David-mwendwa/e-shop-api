7a697e3fc3b208162b66a9b6e74837a3c77d9f03
techdave <klatzdavie19@gmail.com>
2022-04-09 16:06:48 +0300

    add deleteCategory and getAllCategories controllers

--- backend/controllers/categoriesController.js
+++ backend/controllers/categoriesController.js
@@ -17,7 +17,22 @@ const createCategory = async (req, res) => {
 };
 
 const getCategories = async (req, res) => {
-  res.send('getCategories');
+  try {
+    const categories = await Category.find({});
+    res.status(StatusCodes.OK).json({ count: categories.length, categories });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ msg: 'something went wrong' });
+  }
 };
 
-export { createCategory, getCategories };
+const deleteCategory = async (req, res) => {
+  try {
+    let { id: categoryId } = req.params;
+    await Category.findOneAndRemove({ _id: categoryId });
+    res.status(StatusCodes.OK).json({ msg: 'Category deleted successfully' });
+  } catch (error) {
+    res.status(StatusCodes.NOT_FOUND).json({ msg: 'something went wrong' });
+  }
+};
+
+export { createCategory, getCategories, deleteCategory };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 import {
   getCategories,
   createCategory,
+  deleteCategory,
 } from '../controllers/categoriesController.js';
 
 router.route('/').post(createCategory).get(getCategories);
+router.route('/:id').delete(deleteCategory);
 
 export default router;
