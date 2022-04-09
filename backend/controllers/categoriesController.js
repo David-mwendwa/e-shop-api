dfa161f4e9ee25622f3bb3f92b20809e2c4a5147
techdave <klatzdavie19@gmail.com>
2022-04-09 16:18:23 +0300

    improve the controllers

--- backend/controllers/categoriesController.js
+++ backend/controllers/categoriesController.js
@@ -2,18 +2,22 @@ import { StatusCodes } from 'http-status-codes';
 import Category from '../models/Category.js';
 
 const createCategory = async (req, res) => {
-  let category = new Category({
-    name: req.body.name,
-    icon: req.body.icon,
-    color: req.body.color,
-  });
-  category = await category.save();
-  if (!category) {
-    return res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ msg: 'category cannot be created' });
+  try {
+    let category = new Category({
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    });
+    category = await category.save();
+    if (!category) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ success: false, message: 'category cannot be created' });
+    }
+    res.status(StatusCodes.CREATED).json({ category });
+  } catch (error) {
+    res.status(StatusCodes.NOT_FOUND).json({ success: false, eror: error });
   }
-  res.status(StatusCodes.CREATED).json({ category });
 };
 
 const getCategories = async (req, res) => {
@@ -21,17 +25,26 @@ const getCategories = async (req, res) => {
     const categories = await Category.find({});
     res.status(StatusCodes.OK).json({ count: categories.length, categories });
   } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).json({ msg: 'something went wrong' });
+    res.status(StatusCodes.BAD_REQUEST).json({ success: false, error: error });
   }
 };
 
 const deleteCategory = async (req, res) => {
   try {
     let { id: categoryId } = req.params;
-    await Category.findOneAndRemove({ _id: categoryId });
-    res.status(StatusCodes.OK).json({ msg: 'Category deleted successfully' });
+    const category = await Category.findOneAndRemove({ _id: categoryId });
+    if (!category) {
+      res.status(StatusCodes.NOT_FOUND).json({
+        sucess: false,
+        message: `No category with the id ${categoryId}`,
+      });
+    } else {
+      res
+        .status(StatusCodes.OK)
+        .json({ sucess: true, message: 'Category deleted successfully' });
+    }
   } catch (error) {
-    res.status(StatusCodes.NOT_FOUND).json({ msg: 'something went wrong' });
+    res.status(StatusCodes.NOT_FOUND).json({ success: false, eror: error });
   }
 };
 
