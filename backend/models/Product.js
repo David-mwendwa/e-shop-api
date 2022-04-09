f9236451fe4f9b0c93925603fdab5983ceed3602
techdave <klatzdavie19@gmail.com>
2022-04-09 15:32:38 +0300

    build products model

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -5,13 +5,58 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: '',
+  },
   image: {
     type: String,
-    required: false,
+    default: '',
+  },
+  images: [
+    {
+      type: String,
+      default: '',
+    },
+  ],
+  brand: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
   },
   countInStock: {
     type: Number,
     required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
   },
 });
 
