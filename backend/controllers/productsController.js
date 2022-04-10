f3032a1b4d48ce73cd7d2087dba914f3119f76f2
techdave <klatzdavie19@gmail.com>
2022-04-10 21:12:11 +0300

    add updateProduct controller

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -14,20 +14,31 @@ const createProduct = async (req, res) => {
 
 const getProducts = async (req, res) => {
   const products = await Product.find({});
-  res.status(StatusCodes.OK).json({ success: true, products });
+  res.status(StatusCodes.OK).json({ products });
 };
 
 const getProduct = async (req, res) => {
   const { id: productId } = req.params;
-  const product = await Product.findOne({ _id: productId });
+  const product = await Product.findOne({ _id: productId }).populate(
+    'category'
+  );
   if (!product) {
     throw new NotFoundError(`No product with id ${productId}`);
   }
-  res.status(StatusCodes.OK).json({ success: true, product });
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const updateProduct = async (req, res) => {
-  res.send('updateProduct');
+  const { id: productId } = req.params;
+  let product = await Product.findOne({ _id: productId });
+  if (!product) {
+    throw new NotFoundError(`No product with id ${productId}`);
+  }
+  product = await Product.findByIdAndUpdate(productId, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const deleteProduct = async (req, res) => {
