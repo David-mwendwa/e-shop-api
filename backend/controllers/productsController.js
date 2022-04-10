1aa253391a10394c1a304c0b328f3671b0e393f4
techdave <klatzdavie19@gmail.com>
2022-04-10 09:36:48 +0300

    refactor createProduct, getProducts

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -1,35 +1,20 @@
 import { StatusCodes } from 'http-status-codes';
 import Product from '../models/Product.js';
+import { BadRequestError, NotFoundError } from '../errors/index.js';
 
-const createProduct = (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(StatusCodes.CREATED).json(createdProduct);
-    })
-    .catch((error) => {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        error: error.message,
-        success: false,
-      });
-    });
+const createProduct = async (req, res) => {
+  const { name, description, countInStock } = req.body;
+  if (!name || !description || !countInStock) {
+    throw new BadRequestError('Please provide name, description, count');
+  }
+  let product = new Product(req.body);
+  product = await product.save();
+  res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getProducts = async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(StatusCodes.OK).json(products);
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      error: error,
-      success: false,
-    });
-  }
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ success: true, products });
 };
 
 const getProduct = async (req, res) => {
