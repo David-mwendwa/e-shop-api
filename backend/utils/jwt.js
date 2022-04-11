8833eeae6cb2466e01884c45f389714c282f56ea
techdave <klatzdavie19@gmail.com>
2022-04-12 00:07:58 +0300

    add some more

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -8,6 +8,7 @@ const authJWT = () => {
   }).unless({
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
       '/api/v1/auth/register',
       '/api/v1/auth/login',
     ],
