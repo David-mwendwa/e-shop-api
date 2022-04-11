bfafff5bae1e39a0a8bee965041d081d11d3f940
techdave <klatzdavie19@gmail.com>
2022-04-12 00:06:43 +0300

    execlude some REST API routes from authentication

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -5,6 +5,12 @@ const authJWT = () => {
   return expressJWT({
     secret: process.env.JWT_SECRET,
     algorithms: ['HS256'],
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      '/api/v1/auth/register',
+      '/api/v1/auth/login',
+    ],
   });
 };
 
